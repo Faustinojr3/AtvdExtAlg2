499403b7962fb565216ded34cfee7ba109e73986
Faustinojr3 <faustinojunior2020pro@gmail.com>
2022-10-19 18:58:13 -0400

    Atividade 0.0

---
+++ Atividade vetores/Atividade_Vetores/Atividade_Vetores/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_Vetores
+{
+    internal class Class1
+    {
+        public void media()
+        {
+            
+        }
+
+
+
+    }
+}
