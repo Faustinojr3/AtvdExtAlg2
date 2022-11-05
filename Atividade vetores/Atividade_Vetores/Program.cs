70d1d31b83c9c04323e5c5a401a7df19aa738677
Faustinojr3 <faustinojunior2020pro@gmail.com>
2022-11-05 01:39:41 -0400

    Atividade Vetores 4.0

--- Atividade vetores/Atividade_Vetores/Program.cs
+++ Atividade vetores/Atividade_Vetores/Program.cs
@@ -55,6 +55,7 @@ switch (atividadeVetores)
 
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         break;
     case "3":
@@ -126,7 +127,8 @@ switch (atividadeVetores)
         }
 
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
     case "7":
         // 7.	Codifique um algoritmo Histograma, que exiba um histograma da variação da temperatura durante a semana. Por exemplo, se as  temperaturas  forem  19O C,   21 °C,    25°C, 22OC,  20°C,  17°C  e  l 5°C,  de  domingo  a sábado, respectivamente, o algoritmo deve- rá exibir:
         // D   :   ■■■■■■■■■■■■■■■■■■■
@@ -134,69 +136,175 @@ switch (atividadeVetores)
         // T : ■■■■■■■■■■■■■■■■■■■■■■■■■    
         // Suponha que as temperaturas sejam todas positivas e que nenhuma seja maior que 80OC. Dica: crie uma rotina que exiba uma linha, com uma quantidade de caracteres de tamanho proporcional a temperatura
 
+Console.WriteLine("Seja bem vindo ao historagrama por favor insira as tamperaturas dos seguintes dias:");
+
+int [] historagrama = new int [7];
+string [] dias= new string[7];
+
+
+dias [0]="Domingo: ";
+dias [1]="Segunda: ";
+dias [2]="Terça: ";
+dias [3]="Quarta: ";
+dias [4]="Quinta: ";
+dias [5]="Sexta: ";
+dias [6]="Sábado: ";
+
+//--------------Adicionar valores historiograma---------------------------
+for(int hist = 0; hist<dias.Length; hist++){
+
+Console.WriteLine(dias[hist]);
+historagrama[hist]= Convert.ToDouble(Console.ReadLine());
+}
+
+
+
+
+
+//-------------Convertendo para string e demostrando o historiograma----------------------
+for(int hist = 0; hist<historagrama.Length; hist++){
+
+if(historagrama[hist]>= 80 && historagrama[hist]>0) {
+
+
+
+string cp= "";
+// cp.PadRight (Quantidadde minima de caraceres, 'Simbolo a ser usado para completar os requisitos mínimos')
+cp= cp.PadRight(historagrama[hist], '■');
+System.Console.WriteLine(dias[hist]+ cp);
+
+}
+
+}
 
         break;
 
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////
     case "8":
 
         // 8.	Faça um algoritmo que construa dois veto- res A e B de 10 elementos e, a partir de- les, crie um vetor C, composto pela soma dos elementos, sendo: C[1]= A[1] + B[10], C[2) =  A[2) + B[9],
         // C[3]= A[3] + B[8)etc.
 
-        double A []= new double [10];
-        double B[] = new double[10];
-        double C[] = new double[20]; 
+        double [] A = new double [10];
+        double [] B = new double[10];
+        double [] C = new double[10]; 
 
+
+//      --Valores do Vetor A--
         for(int a=0; a<A.Length; a++)
         {
+            int a1 = 1;
+            a1++;
+            Console.WriteLine(" VALORES VETOR A \r\n"+a1+ "º Valor" );
             A[a] = Convert.ToDouble(Console.Readline());
 
 
         }
+ //      --Valores do Vetor B--
+         for(int b=0; b<B.Length; b++)
+        {
+            int b1 = 1;
+            b1++;
+
+            Console.WriteLine(" VALORES VETOR A \r\n"+b1+ "º Valor" );
+            B[b] = Convert.ToDouble(Console.Readline());
+
+
+        }
+//      --Valores do Vetor C--
+        for(int c=0; c<C.Length; c++)
+        {
+            
+            int c2 = 9;
+            c2--;
+
+            C[c]= A[c]+B[c2];
+
+        }
         
+       //      --Resultados  Vetor C--
         
-        
-        
-        
-        
+for(int c=0; c<C.Length; c++){
+p=1;
+p++;
+Console.WriteLine(" Os recultados encontrados no vetor C são: \r\n"+p+ "º Valor encontrado é:\r\n"+ C[c]);
+
+
+}
         
         break;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     case "9":
         // 9.	Elabore um algoritmo que crie dois vetores A e B de 10 elementos e, a partir deles,
         //  crie um vetor C, composto pela uniáo dos elementos de A e B, dispostos em ordem crescente, exibindo o resultado.
 
-        double[] A = new double[10];
-        double[] B = new double[10];
-        double[] C = new double[20];
-        int i = 0;
-        int j = 10;
+        double[] D = new double[20];
+       
         Random aleatorio = new Random();
 
 
-        for (i < A.Length; i++;)
+       //      --Valores do Vetor A--
+        for(int a=0; a<A.Length; a++)
         {
-            j++;
-            A[i] = aleatorio.Next(1, 50);
-            B[i] = aleatorio.Next(1, 50);
-
-            C[i]= A[i];
-            C[j]= B[i];
-
-            if(){
-                
-            }
- 
-
-       }
+            int a1 = 1;
+            a1++;
+            Console.WriteLine(" VALORES VETOR A \r\n"+a1+ "º Valor" );
+            A[a] = Convert.ToDouble(Console.Readline());
 
 
+        }
+ //      --Valores do Vetor B--
+         for(int b=0; b<B.Length; b++)
+        {
+            int b1 = 1;
+            b1++;
+
+            Console.WriteLine(" VALORES VETOR A \r\n"+b1+ "º Valor" );
+            B[b] = Convert.ToDouble(Console.Readline());
+
+
+        }
+
+//      --Valores do Vetor C--
+
+  for(int d=0; d==9; d++){
+
+int d2=10;
+d2++;
+
+
+ D[d]= A[d];
+  D[d2]= B[d];
+    
+  }
+
+ //      --Tratamento valores de C--
 
 
 
 
+
+
+
+
+
+
+
+
+
+      //      --Resultados  Vetor C--
+        
+for(int c=0; c<D.Length; c++){
+p=1;
+p++;
+Console.WriteLine(" Os recultados encontrados no vetor C são: \r\n"+p+ "º Valor encontrado é:\r\n"+ D[c]);
+
+
+}
 
 
 
@@ -204,6 +312,7 @@ switch (atividadeVetores)
 
         break;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     case "10":
         //  10.	A partir de cinco vetores de cinco elementos inteiros, fornecidos pelo usuario, crie uma matriz de cinco linhas e colunas e exiba
